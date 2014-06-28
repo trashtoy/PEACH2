733ff9f0b040bbbc1586698edb6e3acc032926a4
trashtoy <trashtoy@yahoo.co.jp>
2014-06-28 17:34:56 +0900

    Added tests for min() and max() using Comparator

--- test/src/Peach/Util/ArraysTest.php
+++ test/src/Peach/Util/ArraysTest.php
@@ -41,6 +41,7 @@ class ArraysTest extends \PHPUnit_Framework_TestCase
      * - 与えられた配列の中で一番大きな値を返すこと
      * - Comparable を実装しているオブジェクトの場合, compareTo() メソッドを使って大小を比較すること
      * - 空の配列が指定された場合は null を返すこと
+     * - Comparator が指定された場合はその Comparator の仕様に従って比較すること
      * 
      * @covers Peach\Util\Arrays::max
      */
@@ -58,6 +59,10 @@ class ArraysTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($test2[2], Arrays::max($test2));
         
         $this->assertNull(Arrays::max(array()));
+        
+        $comparator = new ArraysTest_Comparator();
+        $result     = Arrays::max(array("three", "one", "five", "two", "four"), $comparator);
+        $this->assertSame("five", $result);
     }
 
     /**
@@ -67,6 +72,7 @@ class ArraysTest extends \PHPUnit_Framework_TestCase
      * - 与えられた配列の中で一番小さな値を返すこと
      * - Comparable を実装しているオブジェクトの場合, compareTo() メソッドを使って大小を比較すること
      * - 空の配列が指定された場合は null を返すこと
+     * - Comparator が指定された場合はその Comparator の仕様に従って比較すること
      * 
      * @covers Peach\Util\Arrays::min
      */
@@ -84,6 +90,10 @@ class ArraysTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($test2[1], Arrays::min($test2));
         
         $this->assertNull(Arrays::max(array()));
+        
+        $comparator = new ArraysTest_Comparator();
+        $result     = Arrays::min(array("three", "one", "five", "two", "four"), $comparator);
+        $this->assertSame("one", $result);
     }
 
     /**
@@ -366,5 +376,34 @@ class ArraysTest_C implements Comparable
         } else {
             throw new Exception();
         }
+    }
+}
+
+class ArraysTest_Comparator implements Comparator
+{
+    /**
+     * @param  string $var1
+     * @param  string $var2
+     * @return int
+     */
+    public function compare($var1, $var2)
+    {
+        return $this->convert($var1) - $this->convert($var2);
+    }
+    
+    /**
+     * @param  string $var
+     * @return int
+     */
+    private function convert($var)
+    {
+        static $names = array(
+            "one"   => 1,
+            "two"   => 2,
+            "three" => 3,
+            "four"  => 4,
+            "five"  => 5,
+        );
+        return array_key_exists($var, $names) ? $names[$var] : 0;
     }
 }
