f9c0adf422f4e69d2cb78baf349770c14e557300
trashtoy <trashtoy@yahoo.co.jp>
2015-05-16 13:14:49 +0900

    Added expression: StructuralChar
    Implemented BNF:
    begin-array     = ws %x5B ws  ; [ left square bracket
    begin-object    = ws %x7B ws  ; { left curly bracket
    end-array       = ws %x5D ws  ; ] right square bracket
    end-object      = ws %x7D ws  ; } right curly bracket
    name-separator  = ws %x3A ws  ; : colon
    value-separator = ws %x2C ws  ; , comma

---
+++ test/Peach/DF/JsonCodec/StructuralCharTest.php
@@ -0,0 +1,71 @@
+<?php
+namespace Peach\DF\JsonCodec;
+
+class StructuralCharTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+    }
+    
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+    
+    /**
+     * インスタンス化直後は $result が null となっていることを確認します.
+     * 
+     * @covers Peach\DF\JsonCodec\StructuralChar::__construct
+     * @covers Peach\DF\JsonCodec\StructuralChar::getResult
+     */
+    public function test__construct()
+    {
+        $expr = new StructuralChar(array(",", "}"));
+        $this->assertNull($expr->getResult());
+    }
+    
+    /**
+     * @covers Peach\DF\JsonCodec\StructuralChar::handle
+     * @covers Peach\DF\JsonCodec\StructuralChar::handleChar
+     * @covers Peach\DF\JsonCodec\StructuralChar::getResult
+     */
+    public function testHandleAndGetResult()
+    {
+        $context = new Context("    }    ");
+        $expr    = new StructuralChar(array(",", "}"));
+        $expr->handle($context);
+        $this->assertSame("}", $expr->getResult());
+    }
+    
+    /**
+     * @covers Peach\DF\JsonCodec\StructuralChar::handle
+     * @expectedException Peach\DF\JsonCodec\DecodeException
+     * @expectedExceptionMessage "Unexpected end of JSON at line 3, column 1"
+     */
+    public function testHandleFailByEndOfJson()
+    {
+        $context = new Context("    \r\n    \n");
+        $expr    = new StructuralChar(array(",", "}"));
+        $expr->handle($context);
+    }
+    
+    /**
+     * @covers Peach\DF\JsonCodec\StructuralChar::handle
+     * @covers Peach\DF\JsonCodec\StructuralChar::handleChar
+     * @expectedException Peach\DF\JsonCodec\DecodeException
+     * @expectedExceptionMessage 'x' is not allowed (expected: ',', '}') at line 3, column 4
+     */
+    public function testHandleFailByInvalidChar()
+    {
+        $context = new Context("    \n        \r   x   \r\n    ");
+        $expr    = new StructuralChar(array(",", "}"));
+        $expr->handle($context);
+    }
+}
