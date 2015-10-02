44c4af48b8398f084c504b6db74f11476f22d39e
trashtoy <trashtoy@yahoo.co.jp>
2015-10-02 18:53:18 +0900

    (WIP) Added class Request

---
+++ src/Peach/Http/Request.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * Copyright (c) 2015 @trashtoy
+ * https://github.com/trashtoy/
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+/**
+ * PHP class file.
+ * @auhtor trashtoy
+ * @since  2.2.0
+ */
+namespace Peach\Http;
+
+class Request
+{
+    /**
+     * クエリ部分を除いたパス文字列です.
+     * @var string
+     */
+    private $path;
+    
+    /**
+     * PHP の $_GET に相当するパラメータです.
+     * @var array
+     */
+    private $queryParameters;
+    
+    /**
+     * PHP の $_POST に相当するパラメータです.
+     * @var array
+     */
+    private $postParameters;
+    
+    /**
+     * HeaderItem 型オブジェクトの配列です.
+     * @var HeaderItem[]
+     */
+    private $headerList;
+    
+    public function __construct()
+    {
+        $this->path = null;
+        $this->queryParameters = array();
+        $this->postParameters  = array();
+        $this->headerList      = array();
+    }
+}
