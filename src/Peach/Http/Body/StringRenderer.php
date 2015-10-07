1d378481b9665de3f98db522c7e4fdcf9ab89c03
trashtoy <trashtoy@yahoo.co.jp>
2015-10-07 21:28:56 +0900

    Added class StringRenderer

---
+++ src/Peach/Http/Body/StringRenderer.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * Copyright (c) 2015 @trashtoy
+ * https://github.com/trashtoy/
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+/**
+ * PHP class file.
+ * @auhtor trashtoy
+ * @since  2.2.0
+ */
+namespace Peach\Http\Body;
+
+use Peach\Http\BodyRenderer;
+use Peach\Util\Values;
+
+/**
+ * 指定された値をそのまま文字列として出力する Renderer です.
+ * このクラスは, メディアタイプが text/plain である Response だけでなく,
+ * 画像や ZIP ファイルといった各種バイナリ形式の Response に使われることも想定しています.
+ */
+class StringRenderer implements BodyRenderer
+{
+    /**
+     * このクラスはシングルトンです.
+     */
+    private function __construct() {}
+    
+    /**
+     * 指定された値を文字列として返します.
+     * 
+     * @param mixed $var
+     * @return string
+     */
+    public function render($var)
+    {
+        return Values::stringValue($var);
+    }
+}
