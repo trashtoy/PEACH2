e2dcc1363e246ff00992047b50ef42a834c609b0
trashtoy <trashtoy@yahoo.co.jp>
2015-12-09 12:21:28 +0900

    Modified PHPdoc comments

--- src/Peach/Http/Header/NoField.php
+++ src/Peach/Http/Header/NoField.php
@@ -1,15 +1,25 @@
 <?php
 
 namespace Peach\Http\Header;
+
 use Peach\Http\HeaderField;
 
+/**
+ * 存在しない HeaderField をあらわします. (Null Object パターン)
+ * 
+ * この HeaderField の getName() および format() は空文字列を返します..
+ */
 class NoField implements HeaderField
 {
+    /**
+     * このクラスを直接インスタンス化することはできません.
+     */
     private function __construct() {}
     
     /**
+     * 空文字列を返します.
      * 
-     * @return string
+     * @return string 空文字列
      */
     public function format()
     {
@@ -17,18 +27,28 @@ class NoField implements HeaderField
     }
     
     /**
-     * @return string
+     * 空文字列を返します.
+     * 
+     * @return string 空文字列
      */
     public function getName()
     {
         return "";
     }
     
+    /**
+     * このフィールドは値を持たないため null を返します.
+     * @return null
+     */
     public function getValue()
     {
         return null;
     }
     
+    /**
+     * このクラスの唯一のインスタンスを返します.
+     * @return NoField 唯一の NoField インスタンス
+     */
     public static function getInstance()
     {
         static $instance = null;
@@ -37,4 +57,5 @@ class NoField implements HeaderField
         }
         return $instance;
     }
+
 }
