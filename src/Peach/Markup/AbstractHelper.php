76f1000fa4ad87400b887c00d6606b84a50083d8
trashtoy <trashtoy@yahoo.co.jp>
2016-10-27 19:30:00 +0900

    Added class AbstractHelper

---
+++ src/Peach/Markup/AbstractHelper.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ * Copyright (c) 2016 @trashtoy
+ * https://github.com/trashtoy/
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+/**
+ * PHP class file.
+ * @auhtor trashtoy
+ * @since  2.2.0
+ */
+namespace Peach\Markup;
+
+abstract class AbstractHelper implements Helper
+{
+    /**
+     *
+     * @var Helper
+     */
+    private $parent;
+    
+    /**
+     * 
+     * @param Helper $parent
+     */
+    public function __construct(Helper $parent)
+    {
+        $this->parent = $parent;
+    }
+    
+    /**
+     * 
+     * @return Helper
+     */
+    public function getParentHelper()
+    {
+        return $this->parent;
+    }
+    
+    /**
+     * ベースの Helper オブジェクトの createElement() を実行します.
+     * 
+     * @param  string  $name 要素名
+     * @return Element       指定された要素名を持つ Element
+     */
+    public function createElement($name)
+    {
+        return $this->parent->createElement($name);
+    }
+    
+    /**
+     * 
+     * @param  string|Component $var
+     * @param  array $attr
+     * @return HelperObject
+     */
+    public function tag($var, $attr = array())
+    {
+        return $this->parent->tag($var, $attr);
+    }
+    
+    /**
+     * 
+     * @param  HelperObject $object
+     * @return mixed
+     */
+    public function write(HelperObject $object)
+    {
+        return $this->parent->write($object);
+    }
+}
