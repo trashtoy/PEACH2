beee23f301a5437e83d0b801b4ef859dbad84a67
trashtoy <trashtoy@yahoo.co.jp>
2016-10-29 22:49:46 +0900

    Added member: $mode

--- src/Peach/Markup/HtmlHelper.php
+++ src/Peach/Markup/HtmlHelper.php
@@ -27,19 +27,160 @@
  */
 namespace Peach\Markup;
 
+use InvalidArgumentException;
+
 class HtmlHelper extends AbstractHelper
 {
 
     /**
+     * @var string
+     */
+    const MODE_HTML4_STRICT        = "html4s";
+    
+    /**
+     * @var string
+     */
+    const MODE_HTML4_TRANSITIONAL  = "html4t";
+    
+    /**
+     * @var string
+     */
+    const MODE_XHTML1_STRICT       = "xhtml1s";
+    
+    /**
+     * @var string
+     */
+    const MODE_XHTML1_TRANSITIONAL = "xhtml1t";
+    
+    /**
+     * @var string
+     */
+    const MODE_XHTML1_1            = "xhtml1_1";
+    
+    /**
+     * @var string
+     */
+    const MODE_HTML5               = "html5";
+    
+    /**
+     *
+     * @var string
+     */
+    private $docType;
+    
+    /**
+     *
+     * @var bool
+     */
+    private $isXhtml;
+    
+    public function __construct(Helper $parent, $docType = null, $isXhtml = null)
+    {
+        parent::__construct($parent);
+        $this->docType = $docType;
+        $this->isXhtml = $isXhtml;
+    }
+    
+    /**
      * 
-     * @param  bool $isXhtml
+     * @param  string $mode
      * @return HtmlHelper
      */
-    public static function newInstance($isXhtml = false)
+    public static function newInstance($mode = null)
     {
+        $actualMode     = self::detectMode($mode);
+        $baseHelper     = self::createBaseHelper($actualMode);
+        $docType        = self::getDocTypeFromMode($actualMode);
+        $isXhtml        = self::checkXhtmlFromMode($actualMode);
+        return new self($baseHelper, $docType, $isXhtml);
+    }
+    
+    /**
+     * 
+     * @param  string $mode
+     * @return HtmlHelper
+     */
+    private static function createBaseHelper($mode)
+    {
+        $isXhtml        = self::checkXhtmlFromMode($mode);
         $builder        = self::createBuilder($isXhtml);
         $emptyNodeNames = self::getEmptyNodeNames();
-        return new self(new BaseHelper($builder, $emptyNodeNames));
+        return new BaseHelper($builder, $emptyNodeNames);
+    }
+    
+    /**
+     * 
+     * @return Component
+     */
+    public function xmlDec()
+    {
+        return $this->isXhtml ? new Code('<?xml version="1.0" encoding="UTF-8"?>') : None::getInstance();
+    }
+    
+    /**
+     * 
+     * @return Component
+     */
+    public function docType()
+    {
+        return strlen($this->docType) ? new Code($this->docType) : None::getInstance();
+    }
+    
+    private static function detectMode($param)
+    {
+        static $mapping = null;
+        if ($mapping === null) {
+            $keys = array(
+                self::MODE_HTML4_TRANSITIONAL,
+                self::MODE_HTML4_STRICT,
+                self::MODE_XHTML1_STRICT,
+                self::MODE_XHTML1_TRANSITIONAL,
+                self::MODE_XHTML1_1,
+                self::MODE_HTML5,
+            );
+            $altMapping = array(
+                ""      => self::MODE_HTML5,
+                "html"  => self::MODE_HTML5,
+                "html4" => self::MODE_HTML4_TRANSITIONAL,
+                "xhtml" => self::MODE_XHTML1_TRANSITIONAL,
+            );
+            $mapping = array_merge(array_combine($keys, $keys), $altMapping);
+        }
+        
+        $key = strtolower($param);
+        if (array_key_exists($key, $mapping)) {
+            return $mapping[$key];
+        } else {
+            throw new InvalidArgumentException("Invalid mode name: {$param}");
+        }
+    }
+    
+    /**
+     * 
+     * @param  string $mode
+     * @return bool
+     */
+    private static function checkXhtmlFromMode($mode)
+    {
+        static $xhtmlModes = array(
+            self::MODE_XHTML1_STRICT,
+            self::MODE_XHTML1_TRANSITIONAL,
+            self::MODE_XHTML1_1,
+        );
+        return in_array($mode, $xhtmlModes);
+    }
+    
+    private static function getDocTypeFromMode($mode)
+    {
+        static $docTypeList = array(
+            self::MODE_HTML4_STRICT        => '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">',
+            self::MODE_HTML4_TRANSITIONAL  => '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">',
+            self::MODE_XHTML1_STRICT       => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
+            self::MODE_XHTML1_TRANSITIONAL => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
+            self::MODE_XHTML1_1            => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">',
+            self::MODE_HTML5               => '<!DOCTYPE html>',
+        );
+        return $docTypeList[$mode];
     }
     
     /**
